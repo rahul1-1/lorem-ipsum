b8067c4cb8d4064c55c992a68725f6bdc2d88b82
Rahul Choudhary <rc9782276591@gmail.com>
2023-03-04 14:39:28 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React, { ChangeEvent, ChangeEventHandler, FormEvent, useState } from 'react';
+import data from './data';
+function App() {
+
+  const [count, setCount] = useState<number>(0);
+  const [text,setText] = useState<string[]>([]);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>):void=>{
+    e.preventDefault();
+    let amount =count;
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  }
+
+  return (
+    <section className="section-center">
+      <h3> tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">
+          paragraphs:
+        </label>
+        <input type="number" name='amount' id='amount'
+         value={count}
+         onChange={(e:ChangeEvent<HTMLInputElement>):void =>{setCount(parseInt(e.target.value))}}
+         />
+         <button type='submit' className='btn'>Generate </button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+      </section>
+    )
+}
+
+export default App;
